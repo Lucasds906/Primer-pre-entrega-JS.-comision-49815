ec263ecccf460ff54e5cfafd9978a4be88b4c9a2
lucas sosa <sosalucas906@gmail.com>
2023-12-21 19:54:51 -0300

    ahorcado

--- main.js
+++ main.js
@@ -1,186 +1,65 @@
-let nombre = prompt('Bienvenido. Para ingresar, escriba su nombre.')
-let nombreUsuario = prompt(`Hola ${nombre}, por favor, cree su nombre de usuario.`)
-let password = prompt('Ahora cree una contraseña alfanumérica')
+// Lista de palabras para el juego
+const PALABRA = ["dinosaurio", "programacion", "desarrollo", "parlante", "exito", "catapulta", "estuario", "maremoto", "cuadrado", "helicoptero", "parangaricutirimicuaro", "fantasma", "universo", "aleatorio", "manuscrito", "hiperbole", "parasito", "barbacoa", "tradicionalmente"]
 
-alert(`Felicidades, ${nombre}, Su cuenta ha sido creada exitosamente. Inicie sesión para operar.`)
 
-do {
-    usuario = prompt('Ingrese su nombre de usuario.')
-    contraseña = prompt('Ingrese su contraseña.')
-} while ((usuario != nombreUsuario) || (contraseña != password));
+// Aquí se selecciona la palabra aleatoriamente
+const PALABRASELECCIONADA = PALABRA[Math.floor(Math.random() * PALABRA.length)]
 
-alert('Ha iniciado sesión. Qué operación desea realizar?')
+//Lista de variables
+let palabraOculta = new Array(PALABRASELECCIONADA.length).fill('_')
+let intentos = 5
+let letrasIncorrectas = []
+let inputValido = /^[a-zA-Z]$/ // Variable para asegurar que el jugador introduzca solo una letra de la 'a' a la 'z'
 
-let saldo = 0
-let puntos = 0
-let productosDeCanje = [[], [], [], [], []]
-let posiblesCanjes = []
+// Inicia el juego
+let nombre = prompt('Bienvenido al juego. Por favor, intruzca su nombre')
 
-class CELULAR {
-    constructor(marca, modelo, espacio, ram, precio, puntos) {
-        this.marca = marca
-        this.modelo = modelo
-        this.espacio = espacio
-        this.ram = ram
-        this.precio = precio
-        this.puntos = puntos
-        this.info = `${marca}, ${modelo}, ${espacio}, ${ram}, ${precio}, ${puntos} puntos.`
+console.log('Hola ' + nombre + ', buena suerte.')
+
+while (intentos && (palabraOculta.join('') !== PALABRASELECCIONADA)) {
+    jugar(prompt('Elige una letra').toLowerCase())
+}
+
+
+// Función para actualizar la palabra adivinada con la letra correcta
+function actualizarPalabra(letra) {
+    for (let i = 0; i < PALABRASELECCIONADA.length; i++) {
+        if (PALABRASELECCIONADA[i] === letra) {
+            palabraOculta[i] = letra
+        }
     }
 }
 
-productosDeCanje[0].push(
-    new CELULAR('Samnsung', 'S10', '128g', '6g', '$150.000', 3000),
-    new CELULAR('Xiamoi', 'Redmi 11', '128', '8g', '$100.000', 2000),
-    new CELULAR('Iphone', '14', '256g', '8g', '$500.000', 10000)
-)
 
-class COCINA {
-    constructor(producto, material, tamaño, precio, puntos) {
-        this.producto = producto
-        this.material = material
-        this.tamaño = tamaño
-        this.precio = precio
-        this.puntos = puntos
-        this.info = `${producto}, ${material}, ${tamaño}, ${precio}, ${puntos} puntos.`
+// Función para jugar
+function jugar(letra) {
+    if (!inputValido.test(letra)) {
+        console.log('Por favor ingresa solo una letra')
+        return
     }
-}
-productosDeCanje[1].push(
-    new COCINA('Olla', 'Acero inoxidable', '30cm de diámetro', '$15.000', 300),
-    new COCINA('Sartén', 'Aluminio', '35 cm de diámetro', '$20.000', 400),
-    new COCINA('Espátula', 'Madera', '40 cm', '$5.000', 100),
-)
 
-class HOGAR {
-    constructor(tipo, color, material, precio, puntos) {
-        this.tipo = tipo
-        this.color = color
-        this.material = material
-        this.precio = precio
-        this.puntos = puntos
-        this.info = `${tipo}, ${color}, ${material}, ${precio}, ${puntos} puntos.`
-    }
-}
-productosDeCanje[2].push(
-    new HOGAR('Cortina', 'Negro', 'Lino', '$10.000', 200),
-    new HOGAR('Almohadón', 'Beige', 'plumas', '$25.000', 500),
-    new HOGAR('Silla', 'Blanco', 'Madera', '$18.000', 360),
-)
-
-class INDUMENTARIA {
-    constructor(prenda, genero, color, talle, precio, puntos) {
-        this.prenda = prenda
-        this.genero = genero
-        this.color = color
-        this.talle = talle
-        this.precio = precio
-        this.puntos = puntos
-        this.info = `${prenda}, ${genero}, ${color}, ${talle}, ${precio}, ${puntos} puntos.`
-    }
-}
-productosDeCanje[3].push(
-    new INDUMENTARIA('Remera', 'Hombre', 'Negro', '44', '$12.000', 240),
-    new INDUMENTARIA('Pantalón', 'Mujer', 'Jean', '38', '$10.000', 200),
-    new INDUMENTARIA('Buzo', 'Unisex', 'Azul', '40', '$19.000', 380)
-)
-
-class TECNOLOGIA {
-    constructor(producto, marca, color, precio, puntos) {
-        this.producto = producto
-        this.marca = marca
-        this.color = color
-        this.precio = precio
-        this.puntos = puntos
-        this.info = `${producto}, ${marca}, ${color}, ${precio}, ${puntos} puntos.`
-    }
-}
-productosDeCanje[4].push(
-    new TECNOLOGIA('Auriculares', 'Samsung', 'Negro', '$24.000', 480),
-    new TECNOLOGIA('Mouse gamer', 'Trust', 'Negro y rojo', '$30.000', 600),
-    new TECNOLOGIA('Webcam', 'Philips', 'Negro', '$50.000', 1000),
-)
-
-seleccionarOpcion()
-
-// ***************** FUNCIONES******************
-
-function seleccionarOpcion() {
-    let menu = Number(prompt('1. Consultar saldo. 2. realizar un depósito. 3. Extraer dinero. 4. Canjear puntos. 5. Cerrar sesión.'))
-
-    if (menu == 1) {
-        alert(`El saldo de tu cuenta es de $ ${saldo}`)
-        let respuesta = confirm('Desea volver al menú anterior?')
-        if (respuesta) {
-            seleccionarOpcion()
-        } else {
-            return
-        }
-    } else if (menu == 2) {
-        let deposito = Number(prompt('Ingrese la cantidad de dinero que desea depositar.'))
-        if ((deposito <= 0) || (isNaN(deposito))) {
-            alert('Por favor, debe ingresar un numero mayor que 0.')
-            seleccionarOpcion()
-        } else {
-            alert(`Su depósito se efectuó correctamente. El saldo de su cuenta es de $ ${saldo += deposito}.`)
-            puntos += deposito * 0.1
-        }
-        let respuesta = confirm('Desea volver al menú anterior?')
-        if (respuesta) {
-            seleccionarOpcion()
-        } else {
-            return
-        }
-    } else if (menu == 3) {
-        let extraccion = Number(prompt('Ingresa el monto que deseas extraer'))
-        if ((extraccion <= 0) || (isNaN(extraccion))) {
-            alert('Por favor, ingrese un monto mayor que 0')
-            seleccionarOpcion()
-        } else if (extraccion > saldo) {
-            alert('El saldo de tu cuenta es insuficinte para realizar la extracción. Debe ingresar un monto menor.')
-            seleccionarOpcion()
-        } else {
-            alert(`La extración se realizó exitosamente. El saldo de tu cuenta ahora es de $ ${saldo -= extraccion}`)
-        }
-        let respuesta = confirm('Desea volver al menú anterior?')
-        if (respuesta) {
-            seleccionarOpcion()
-        } else {
-            return
-        }
-    } else if (menu == 4) {
-        alert(`Llevas acumulados ${puntos} puntos`)
-        for (contenedor in productosDeCanje) {
-            for (let i = 0; i < productosDeCanje[contenedor].length; i++) {
-                if (puntos >= productosDeCanje[contenedor][i].puntos) {
-                    console.log(`Puedes canjear tus puntos por ${productosDeCanje[contenedor][i].info}.`)
-                    posiblesCanjes.push(productosDeCanje[contenedor][i])
-                } else if (puntos < 100) {
-                    alert('Tus puntos son insuficientes para realizar canjes. Por favor, ingrese más plata para incrementar sus puntos.')
-                    seleccionarOpcion()
-                }
-            }
-        }
-        for (elementos in posiblesCanjes) {
-            let seleccion = prompt(`Para seleccionar el producto ${posiblesCanjes[elementos].info}, presiona ${elementos}. Tenés ${puntos}. Presioná "enter" para pasar al siguiente producto.`)
-            if (seleccion == elementos) {
-                puntos -= posiblesCanjes[elementos].puntos
-                alert(`Felicidades! Has canjeado tus puntos por ${posiblesCanjes[elementos].info}. Tus puntos restantes son ${puntos}`)
-                posiblesCanjes = []
-                seleccionarOpcion()
-            } 
-        }
-        posiblesCanjes = []
-        let respuesta = confirm('Desea volver al menú anterior?')
-        if (respuesta) {
-            seleccionarOpcion()
-        } else {
-            return
-        }
-    } else if (menu == 5) {
-        alert('Gracias por confiar en nosotros, su sesión ha sido cerrada.')
-            return
+    if (palabraOculta.includes(letra) || letrasIncorrectas.includes(letra)) {
+        console.log('Ya has intentado con esa letra. Intenta con otra.')
+    } else if (PALABRASELECCIONADA.includes(letra)) {
+        actualizarPalabra(letra)
+        console.log('Muy bien! La letra está en la palabra.')
     } else {
-        alert('La opción ingresada no es válida.')
-        seleccionarOpcion()
+        letrasIncorrectas.push(letra)
+        intentos--
+        console.log('Oops! La letra no está en la palabra.')
+    }
+
+    // Mostrar estado de la partida
+    console.log(`Palabra: ${palabraOculta.join(' ')}`)
+    console.log(`Te quedan ${intentos} intentos`)
+    console.log(`Letras incorrectas: ${letrasIncorrectas.join(', ')}`)
+    console.log(`   *************   `)
+
+    // Verificar si el jugador ganó o perdió
+    if (palabraOculta.join('') === PALABRASELECCIONADA) {
+        console.log('Felicidades! Has adivinado la palabra.')
+    } else if (!intentos) {
+        console.log('Lo siento. No has adivinado la palabra. La palabra era:', PALABRASELECCIONADA)
     }
 }
 
